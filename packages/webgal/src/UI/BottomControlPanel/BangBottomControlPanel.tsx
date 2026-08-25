5ed0aa698f3b73afccb77fa5809e79fa8b8879bf
boomwwww <boom_w_w@outlook.com>
2026-08-26 04:02:24 +0800

    feat: Bang风格控制面板添加流程图按钮

--- packages/webgal/src/UI/BottomControlPanel/BangBottomControlPanel.tsx
+++ packages/webgal/src/UI/BottomControlPanel/BangBottomControlPanel.tsx
@@ -27,6 +27,7 @@ import {
   ReplayMusic,
   Save,
   SettingTwo,
+  TreeDiagram,
   Unlock,
   LeftTwo,
   RightTwo,
@@ -57,6 +58,7 @@ export function BangBottomControlPanel() {
   }
   const { isSupported: isFullscreenSupport, isFullScreen, toggle: toggleFullscreen } = useFullScreen();
   const GUIStore = useSelector((state: RootState) => state.GUI);
+  const enableFlowchart = useSelector((state: RootState) => state.userData.globalGameVar.Enable_flowchart === true);
   const stageState = useStageState();
   const dispatch = useDispatch();
   const setComponentVisibility = (component: keyof componentsVisibility, visibility: boolean) => {
@@ -191,6 +193,16 @@ export function BangBottomControlPanel() {
               setComponentVisibility('showTextBox', false);
             }}
           />
+          {enableFlowchart && (
+            <PanelButton
+              icon={<TreeDiagram />}
+              text={t('buttons.flowchart')}
+              onClick={() => {
+                setMenuPanel(MenuPanelTag.Flowchart);
+                setComponentVisibility('showMenuPanel', true);
+              }}
+            />
+          )}
           <PanelButton
             icon={<DoubleDown />}
             text={t('buttons.quicklySave')}
